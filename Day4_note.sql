4ac393409bdb8eaecc878f653c2e6a77d108d786
Vyhahaa <154458165+Vyhahaa@users.noreply.github.com>
2023-12-23 16:44:43 +0700

    Create Day4_note.sql

---
+++ Day4_note.sql
@@ -0,0 +1,25 @@
+-- GROUP BY
+select film_id, title,
+max (replacement_cost) as max_cost,
+min (replacement_cost) as min_cost,
+round(avg (replacement_cost),2) as avg_cost,
+sum (replacement_cost) as total_cost
+from film
+group by film_id, title
+
+-- HAVING: mệnh đề điều kiện sau khi tổng hợp dữ liệu, luôn đi sau GROUP BY. Trong khi WHERE là mệnh đề điều kiện cho dữ liệu có sẵn, chưa qua tổng hợp
+select customer_id,
+sum (amount) as total_amount
+from payment
+group by customer_id
+having sum (amount) > 100
+
+-- HAVING challenge & solution
+select customer_id, date(payment_date),
+round(avg (amount),2) as avg_amount,
+count (payment_id) as number_of_payment
+from payment
+where date(payment_date) in ('2020-04-28','2020-04-29','2020-04-30')
+group by customer_id, date(payment_date)
+having count(payment_id) > 1
+order by avg_amount desc
