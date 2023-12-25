e47b3443407bcfb8d1fb48fcd4347503d63492b2
Vyhahaa <154458165+Vyhahaa@users.noreply.github.com>
2023-12-26 00:01:27 +0700

    Update Day5_practice.sql

--- Day5_practice.sql
+++ Day5_practice.sql
@@ -1,1 +1,11 @@
+--Ex1: Mod function returns the remainder of a number divided by another number => Mod (ID,2) = 0 [even]
+select distinct city from station
+where mod(id,2) = 0;
 
+--Ex2
+select (count (city) - count (distinct city)) from station;
+
+--Ex3
+SELECT ROUND(AVG(salary)-AVG(REPLACE(salary,'0','')),0) FROM EMPLOYEES;
+
+--Ex4
