ce4676de4c0ab8332415cbc49a30020c1beacb38
Vyhahaa <154458165+Vyhahaa@users.noreply.github.com>
2023-12-26 01:41:14 +0700

    Update Day5_practice.sql

--- Day5_practice.sql
+++ Day5_practice.sql
@@ -8,6 +8,14 @@ select (count (city) - count (distinct city)) from station;
 --Ex3
 SELECT ROUND(AVG(salary)-AVG(REPLACE(salary,'0','')),0) FROM EMPLOYEES;
 
---Ex4
-SELECT sum(item_count*order_occurrences) / sum(item_count)
+--Ex4: CAST function converts a value (of any type) into a specified datatype
+SELECT round(cast(sum(item_count*order_occurrences) / sum(order_occurrences) as decimal),1)
 FROM items_per_order;
+
+--Ex5
+SELECT candidate_id from candidates
+where skill in ('Python','Tableau','PostgreSQL')
+group by candidate_id
+having count(skill)=3;
+
+--Ex6
